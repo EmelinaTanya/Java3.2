57e4b11d5c3a14c57dfbd61d9ee8ad123f7e8b5b
EmelinaTanya <62333591+EmelinaTanya@users.noreply.github.com>
2020-04-05 16:45:51 +0400

    Update Main.java
    fixed cod

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        long current_account = 100;//текущий остаток
-        long bonus_limit = 1000;//лимит бонусного поплнения
+        long currentAccount = 100;//текущий остаток
+        long bonusLimit = 1000;//лимит бонусного поплнения
         long refill;// сумма пополнения
 
         Scanner scanner = new Scanner(System.in);// создаем объект утилиты сканера для считывания ввода пользователем числа
 
         System.out.print ( "У вас 100 рублей, при пополнении свыше 1000 руб. вы получите 1 рубль за каждые 100 рублей. На сколько поплнить счет? " );
         refill = scanner.nextInt();// ждем ввод числа
-        if ( refill < bonus_limit ) {// если пополнение меньше лимита то бонуса нет
-            current_account += refill;
+        if ( refill < bonusLimit ) {// если пополнение меньше лимита то бонуса нет
+            currentAccount += refill;
         } else {
-            current_account += refill + refill / 100;// иначе  добавляем сумму пополнения и по рублю на каждые 100 рублей целочисленным делением
+            currentAccount += refill + refill / 100;// иначе  добавляем сумму пополнения и по рублю на каждые 100 рублей целочисленным делением
         }
         System.out.print ( "Ваш счет успешно поплнен. Ваш баланс: " );
-        System.out.println ( current_account );
+        System.out.println ( currentAccount );
     }
 }
